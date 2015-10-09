78ef1336575472314910006ce729e9c375e54362
makisyu <makisyu@users.noreply.github.com>
2015-10-09 16:27:06 +0900

    Clean up

--- init.el
+++ init.el
@@ -1,15 +1,20 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (set-face-attribute 'default nil
-                    :family "Inconsolata"
-                    :height 100)
+                    :family "Ricty"
+                    :height 120)
 
-(custom-set-faces '(which-func ((t nil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-info ((t (:underline t))))
+ '(flycheck-warning ((t (:underline t))))
+ '(which-func ((t nil))))
 
 (if (eq window-system 'x)
     (progn (set-scroll-bar-mode 'right)
@@ -46,9 +51,7 @@
 ;; color-theme
 (if (> emacs-major-version 23)
     (if (or (eq window-system 'x) (eq window-system 'w32))
-        (progn (add-to-list 'custom-theme-load-path
-			    "~/.emacs.d/emacs-color-theme-solarized")
-               (load-theme 'solarized t)
+        (progn (load-theme 'solarized t)
                (set-frame-parameter nil 'background-mode 'dark)
                (enable-theme 'solarized))))
 
@@ -61,7 +64,13 @@
 (semantic-gcc-setup)
 ;; ecb
 (setq stack-trace-on-error t)
-(custom-set-variables '(ecb-options-version "2.40"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40")
+ '(safe-local-variable-values (quote ((c-indent-level . 4)))))
 ;; auto-complete
 (add-to-list 'load-path "~/.emacs.d/auto-complete")
 (when (require 'auto-complete-config nil t)
@@ -74,18 +83,20 @@
 
 
 ;; mozc
-(when (require 'mozc nil t)
-      (setq default-input-method "japanese-mozc"))
+(require 'mozc)
+(setq default-input-method "japanese-mozc")
+(require 'mozc-popup)
+(setq mozc-candidate-style 'popup)
 
 
 ;; markdown-mode
-(when (require 'markdown-mode nil t)
-      (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode)))
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
 
 
 ;; yaml-mode
-(when (require 'yaml-mode nil t)
-      (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
 
 ;; Rinari for Ruby on Rails
@@ -94,8 +105,8 @@
 
 ;; show diff by git-gutter
 (add-to-list 'load-path "~/.emacs.d/emacs-git-gutter")
-(when (require 'git-gutter nil t)
-      (global-git-gutter-mode +1))
+(require 'git-gutter)
+(global-git-gutter-mode +1)
 
 
 ;; flycheck
@@ -106,17 +117,17 @@
                          "/usr/include/glib-2.0"
                          "/usr/lib64/glib-2.0/include"
                          "/usr/include/pixman-1"))))
-(custom-set-faces
-  '(flycheck-info ((t (:underline t))))
-  '(flycheck-warning ((t (:underline t)))))
+
 
 
 ;; cscope
-(when (require 'xcscope nil t)
-      (progn (cscope-setup)
-             (setq cscope-do-not-update-database t)
-             (add-hook 'python-mode-hook (function cscope-minor-mode))
-             (add-hook 'erlang-mode (function cscope-minor-mode))))
+(require 'xcscope nil t)
+(cscope-setup)
+(setq cscope-do-not-update-database t)
+(add-hook 'c-mode-hook (function cscope-minor-mode))
+(add-hook 'c++-mode-hook (function cscope-minor-mode))
+(add-hook 'python-mode-hook (function cscope-minor-mode))
+(add-hook 'erlang-mode (function cscope-minor-mode))
 
 
 ;; EDTS
@@ -125,10 +136,11 @@
 
 
 ;; highlight-symbol
-(when (require 'highlight-symbol nil t)
-      (progn (add-hook 'c-mode-hook 'highlight-symbol-mode)
-             (add-hook 'c++-mode-hook 'highlight-symbol-mode)
-             (add-hook 'python-mode-hook 'highlight-symbol-mode)))
+(require 'highlight-symbol nil t)
+(add-hook 'c-mode-hook 'highlight-symbol-mode)
+(add-hook 'c++-mode-hook 'highlight-symbol-mode)
+(add-hook 'python-mode-hook 'highlight-symbol-mode)
+(add-hook 'erlang-mode 'highlight-symbol-mode)
 
 
 ;; smart-parens
--- modules.sh
+++ modules.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -eux
 
-git clone https://github.com/sellout/emacs-color-theme-solarized.git
 git clone https://github.com/auto-complete/auto-complete.git
 git clone https://github.com/syohex/emacs-git-gutter.git
 git clone https://github.com/flycheck/flycheck.git
